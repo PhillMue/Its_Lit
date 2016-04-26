8b9efdc53d0a7ab74a83a3f6d5eec43520d7754f
PhillMue <muenzuve@gmail.com>
2016-04-26 06:35:58 +0300

    declared variables in the main activity

--- app/src/main/java/com/android/mue/itslit/MainActivity.java
+++ app/src/main/java/com/android/mue/itslit/MainActivity.java
@@ -1,13 +1,21 @@
 package com.android.mue.itslit;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.widget.ImageButton;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends Activity {
+    private ImageButton btnSwitch;
+    private Camera camera;
+    private boolean isFlashOn;
+    private boolean hasFlash;
+    private Parameters params;
+    private MediaPlayer mp;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+// flash switch button
+        btnSwitch = (ImageButton) findViewById(R.id.btnSwitch);
     }
-}
