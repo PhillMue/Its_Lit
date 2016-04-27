cc01f489e10670bc237fbe16de153b0c5e73e14e
PhillMue <muenzuve@gmail.com>
2016-04-27 12:55:37 +0300

    added images

--- app/src/main/java/com/android/mue/itslit/MainActivity.java
+++ app/src/main/java/com/android/mue/itslit/MainActivity.java
@@ -1,8 +1,14 @@
 package com.android.mue.itslit;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.hardware.Camera;
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.ImageButton;
 
 public class MainActivity extends Activity {
@@ -10,12 +16,47 @@ public class MainActivity extends Activity {
     private Camera camera;
     private boolean isFlashOn;
     private boolean hasFlash;
-    private Parameters params;
+    private Camera.Parameters params;
     private MediaPlayer mp;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 // flash switch button
         btnSwitch = (ImageButton) findViewById(R.id.btnSwitch);
+
+
+    hasFlash = getApplicationContext().getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA_FLASH);
+
+    if(!hasFlash){
+// device doesn't support flash
+// Show alert message and close the application
+        AlertDialog alert = new AlertDialog.Builder(MainActivity.this)
+                .create();
+        alert.setTitle("Error");
+        alert.setMessage("Sorry, your device doesn't support flash light!");
+        alert.setButton("OK", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+// closing the application
+                finish();
+            }
+        });
+        alert.show();
+        return;
     }
+    }
+
+    // getting camera parameters
+    @SuppressLint("LongLogTag")
+    private void getCamera() {
+        if (camera == null) {
+            try {
+                camera = Camera.open();
+                params = camera.getParameters();
+            } catch (RuntimeException e) {
+                Log.e("Camera Error. Failed to Open. Error: ", e.getMessage());
+            }
+        }
+    }
+}
