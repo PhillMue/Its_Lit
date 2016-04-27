0377773c143370e13bac058d4b553f6222f4c319
PhillMue <muenzuve@gmail.com>
2016-04-27 14:01:35 +0300

    added function for turning on camera flash

--- app/src/main/java/com/android/mue/itslit/MainActivity.java
+++ app/src/main/java/com/android/mue/itslit/MainActivity.java
@@ -1,54 +1,58 @@
 package com.android.mue.itslit;
 
-import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.pm.PackageManager;
 import android.hardware.Camera;
+import android.hardware.Camera.Parameters;
 import android.media.MediaPlayer;
+import android.media.MediaPlayer.OnCompletionListener;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.ImageButton;
 
 public class MainActivity extends Activity {
-    private ImageButton btnSwitch;
+
+    ImageButton btnSwitch;
+
     private Camera camera;
     private boolean isFlashOn;
     private boolean hasFlash;
-    private Camera.Parameters params;
-    private MediaPlayer mp;
+    Parameters params;
+    MediaPlayer mp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-// flash switch button
+
+        // flash switch button
         btnSwitch = (ImageButton) findViewById(R.id.btnSwitch);
 
-
+    }
     hasFlash = getApplicationContext().getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA_FLASH);
 
-    if(!hasFlash){
-// device doesn't support flash
-// Show alert message and close the application
+    if (!hasFlash) {
+        // device doesn't support flash
+        // Show alert message and close the application
         AlertDialog alert = new AlertDialog.Builder(MainActivity.this)
                 .create();
         alert.setTitle("Error");
         alert.setMessage("Sorry, your device doesn't support flash light!");
         alert.setButton("OK", new DialogInterface.OnClickListener() {
             public void onClick(DialogInterface dialog, int which) {
-// closing the application
+                // closing the application
                 finish();
             }
         });
-        alert.show();
         return;
-    }
+        alert.show();
+
     }
 
     // getting camera parameters
-    @SuppressLint("LongLogTag")
     private void getCamera() {
         if (camera == null) {
             try {
@@ -59,4 +63,26 @@ public class MainActivity extends Activity {
             }
         }
     }
-}
+
+    /*
+    * Turning On flash
+    */
+    private void turnOnFlash() {
+        if (!isFlashOn) {
+            if (camera == null || params == null) {
+                return;
+            }
+            // play sound
+            playSound();
+
+            params = camera.getParameters();
+            params.setFlashMode(Parameters.FLASH_MODE_TORCH);
+            camera.setParameters(params);
+            camera.startPreview();
+            isFlashOn = true;
+
+            // changing button/switch image
+            toggleButtonImage();
+        }
+
+    }
